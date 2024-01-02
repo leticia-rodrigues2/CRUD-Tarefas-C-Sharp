34e9667dc16e30782f0376b9cbae9e09c892629e
Leticia Rodrigues Bento <leticia.bento@vocedm.com.br>
2024-01-01 22:32:49 -0300

    first commit

---
+++ Data/Map/UsuarioMap.cs
@@ -0,0 +1,6 @@
+﻿namespace SistemaDeTarefas.Data.Map
+{
+    public class UsuarioMap
+    {
+    }
+}
---
+++ Data/SistemadeTarefasDBContex.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaDeTarefas.Conrollers;
+using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Data
+{
+    public class SistemadeTarefasDBContex : DbContext
+    {
+        public SistemadeTarefasDBContex(DbContextOptions<SistemadeTarefasDBContex> options)
+            :base(options) 
+        { 
+        }
+
+        public DbSet<UsuarioModel> Usuarios { get; set; }   
+        public DbSet<TarefaModel> Tarefa { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
